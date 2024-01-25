4291a269a5294a8931e8d7e0bab9e896ce40ee58
adrian.torion12@gmail.com <adrian.torion12@gmail.com>
2024-01-25 09:57:52 -0600

    Done finishng the episode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
-import names from './names';
-import User from './user.js';
+import { useState } from "react";
 
 function App() {
+  const [number, newNamber] = useState(0);
+  const [textColor, newTextColor] = useState("black");
+  const [text, newText] = useState(true);
+
+  const color = () => {
+    newTextColor(textColor === "black" ? "Red" : "black");
+  }
+  const ShowHide = () => {
+    newText(!text);
+  }
+  const Increase = () => {
+    newNamber((prevNum) => prevNum + 1);
+  }
+  const Decrease = () => {
+    newNamber((prevNum) => prevNum - 1);
+  }
+  const zero = () => {
+    newNamber(0);
+  }
 
   return (
-
     <div className="App">
-      <User name={names} />
+      <button onClick={Increase}> Increase</button>
+      <button onClick={Decrease}> Decrease </button>
+      <button onClick={zero}> Set to zero</button>
+      <button onClick={color}> Change color </button>
+      <button onClick={ShowHide}>Show/Hide</button>
+      {text && <h1 style={{ color: textColor }}>{number}</h1>}
     </div>
   );
 }
