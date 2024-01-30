ac22d5a9b69899b96315835b95dbc7f5cd968fe6
adrian.torion12@gmail.com <adrian.torion12@gmail.com>
2024-01-30 08:24:13 -0600

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 // App.js
-import Axios from 'axios';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
+import { About } from './About';
+import { Menu } from './Menu';
+import { Navbar } from './Navbar';
+
 
 
 function App() {
-
-  const [fact, getFact] = useState("");
-
-  const getFactData = (title) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${title}/`).then((res) => {
-      console.log(res.data);
-      getFact(res.data[0].excuse);
-    });
-  }
   return (
     <div className="App">
-      <h1>{fact}</h1>
-      <button onClick={() => getFactData("family")}>Family</button>
-      <button onClick={() => getFactData("office")}>Office</button>
-      <button onClick={() => getFactData("family")}>children</button>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='*' element={<h1>404 Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
